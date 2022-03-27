5657234e8b23fce412c1785e62c2cdf03146b94c
Alawon1 <alawon12@gmail.com>
2022-03-27 21:25:09 +0200

    laser movement and destruction after leving screen

--- SpaceInvaders/Code/player.py
+++ SpaceInvaders/Code/player.py
@@ -40,9 +40,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.right = self.max_x_constraint
 
     def shoot_laser(self):
-        self.lasers.add(Laser(self.rect.center))
+        self.lasers.add(Laser(self.rect.center, self.rect.bottom))
 
     def update(self):
         self.get_input()
         self.constraint()
         self.recharge()
+        self.lasers.update()
